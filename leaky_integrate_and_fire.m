4559a684d17a378ae10eeeb5e3530c4ac2220dbb
edgartandberg <154289064+edgartandberg@users.noreply.github.com>
2024-09-16 09:42:16 +0200

    Add files via upload
    can adjust refractory time and length of input current

--- leaky_integrate_and_fire.m
+++ leaky_integrate_and_fire.m
@@ -4,16 +4,16 @@ close all
 clc
 
 
-tau_m = 10; % time constant in ms
-t_ref = 5; % refractory period in ms
-N = 500; % number of measurements
+tau_m = 5; % time constant in ms
+t_ref = 30; % refractory period in ms
+N = 1000; % number of measurements
 I_0 = 2e-6; % input current
 Iext = zeros(1,N); % initialize membrane current
 R = 10^7 ;% resistance in ohms
 
 
 u_rest = -70 ;% resting membrane potential in mV
-u_th = -40 ;% spiking threshold
+u_th = -55 ;% spiking threshold
 u_hp = -90 ;% hyperpolarization
 u_spike = 20 ;% membrane potential at spike peak
 
@@ -24,12 +24,13 @@ t = linspace(-200, 800, N); % time series
 
 %%
 
-num_start = 0.2 * N;
-num_end   = 0.22 * N;  
+num_start = 0.20 * N;
+num_end   = 0.3* N;  
 num_width = round(num_end - num_start);
 Iext(num_start : num_start + num_width) = I_0;
 
 RI = R.*Iext;
+
 
 
 
@@ -38,6 +39,20 @@ RI = R.*Iext;
 %du/dt = (-u + u_rest + R*Iext)/tau_m
 
 u = u_rest + RI.*(1-exp(-(t/tau_m))); %solution for above equation for u at u(0) = u_rest
+
+% Loops over time steps, spikes if threshold is reached, before hyperpolarizing and returning to resting potential
+
+
+
+for ii = 1 : num_end
+    
+    if u(ii+1) > u_th
+       u(ii) = u_spike;
+       u(ii+1) = u_hp;
+       u(ii+2:ii+2+t_ref) = u_rest;
+    end
+end
+
 
 
 %% Plotting input current and voltage
@@ -48,7 +63,8 @@ plot(t,Iext,'r','LineWidth',3)
 
 title('Input current')
 xlabel('time')
-ylabel('I_0')
+ylabel('I_ext')
+xlim([-100 num_end*1.05])
 ylim([0 I_0])
 yline(0)
 
@@ -57,4 +73,6 @@ plot(t,u, 'b', 'LineWidth',2)
 title('Voltage')
 xlabel('time')
 ylabel('u(t)')
-ylim([u_hp u_spike])
+xlim([-100 num_end*1.05])
+ylim([u_hp-10 u_spike+5])
+yline(u_th,'--k','Threshold')
