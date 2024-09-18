83f3ff7badd1d75145a5ead8f81524821b54e014
edgartandberg <154289064+edgartandberg@users.noreply.github.com>
2024-09-18 19:51:58 +0200

    Added optional oscillatory input, to current

--- leaky_integrate_and_fire.m
+++ leaky_integrate_and_fire.m
@@ -1,4 +1,6 @@
 
+%tic
+
 clear all
 close all
 clc
@@ -7,7 +9,7 @@ clc
 
 tau_m = 5; % time constant in ms
 t_ref = 10; % refractory period in ms, change for adjusting spike frequency
-N = 5000; % number of measurements
+N = 10000; % number of measurements
 Iext = zeros(1,N); % initialize membrane current
 I_0 = 1.4e-6; % input current
 R = 10^7; % resistance in ohms
@@ -28,12 +30,27 @@ t = linspace(-0.2*N, 0.8*N, N); % time series
 
 noise = 5*1e-8*randn(size(Iext)); % noise when current is injected
 
-num_start = 0.20 * N; % start of input current
-num_end   = 0.6* N;  % end of input current
+ 
+
+num_start = 0.2 * (N); % start of input current
+num_end   = 0.8 * (N);  % end of input current
 num_width = round(num_end - num_start);
+
+num_pulses = 8; % number of pulses
+
+length_pulse = round(num_width / num_pulses);
+
+
+
 Iext(num_start : num_start + num_width) = I_0;
 Iext = Iext + noise;
 Iext(1:num_start) = 0; % resetting signal before and after current to 0
+
+% add or remove 50 - 53 for oscillatory input
+Iext(num_start + length_pulse : num_start + 2 * length_pulse) = 0;
+Iext(num_start + 3 * length_pulse : num_start + 4 * length_pulse) = 0;
+Iext(num_start + 5 * length_pulse : num_start + 6 * length_pulse) = 0;
+Iext(num_start + 7 * length_pulse : num_start + 8 * length_pulse) = 0;
 Iext(num_end:end) = 0;
 
 
@@ -53,12 +70,12 @@ u = u_rest + RI.*(1-exp(-(t/tau_m))); %solution for above equation for u at u(0)
 
 
 a = t_ref;
-for ii = 1 : num_end
-    if u(ii+1) > u_th
-       u(ii) = u_spike;
-       u(ii+1) = u_hp;
-       u(ii+2:ii+2+a) = u_rest;
-       a = round(a*a_gain); % incrementing by adaptation gain
+for ii = 1 : num_end - 2 - a % Adjust loop range to avoid out-of-bounds errors
+    if u(ii + 1) > u_th
+        u(ii) = u_spike;
+        u(ii + 1) = u_hp;
+        u(ii + 2 : ii + 2 + a) = u_rest;
+        a = round(a * a_gain); % Increment by adaptation gain
     end
 end
 
@@ -88,3 +105,4 @@ xlim([-0.1*N num_end*1.05])
 ylim([u_hp-10 u_spike+5])
 yline(u_th,'--k','Threshold')
 
+%toc
