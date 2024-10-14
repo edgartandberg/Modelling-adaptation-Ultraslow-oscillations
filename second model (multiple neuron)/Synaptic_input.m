cbc5ec15a1276ce8dda43f15208cc0bd9a71bf97
edgartandberg <154289064+edgartandberg@users.noreply.github.com>
2024-10-14 21:58:05 +0200

    pulsating sequence 🤞🏻🤞🏻🤞🏻

--- second model (multiple neuron)/Synaptic_input.m
+++ second model (multiple neuron)/Synaptic_input.m
@@ -1,6 +1,9 @@
-function [u,a,spikecount] = Synaptic_input(previous_chain,u,t_ref,u_rest,num_end,u_th,u_spike,num_start,u_hp,a_gain,weight)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [u,a,spikecount] = Synaptic_input(previous_chain,u,t_ref,u_rest,num_end,u_th,u_spike,current_start,u_hp,a_gain,weight)
+% Synaptic input for generating sequence
+% -------------------------
+% Takes in previous chain from u_all with weight from conn.matrix 
+% for generating the next chain in sequence
+% -------------------------
 
 
 
@@ -8,17 +11,18 @@ a = t_ref;
 spikecount = zeros(1, num_end - 2 - a);
 
 
-u = previous_chain;
+u = previous_chain*weight;
+u = [zeros(1, current_start*0.1), u(1:end-current_start*0.1)];
 
 
-for ii = 1 : num_end - 2 - a % Adjust loop range to avoid out-of-bounds errors
+for ii = current_start: num_end - 2 - a % Adjust loop range to avoid out-of-bounds errors
     if u(ii:ii+1000) < u_th % if below threshold for 1000 ms, resets adaptation
         a = t_ref;
     end
 
     if u(ii + 1) > u_th
         u(ii) = u_spike;
-        spikecount(ii-num_start) = 1;
+        spikecount(ii-current_start) = 1;
         u(ii + 1) = u_hp;
         u(ii + 2 : ii + 2 + a) = u_rest;
         a = round(a + a_gain); % Increment by adaptation gain
--- second model (multiple neuron)/plotConnectivityMatrix.m
+++ second model (multiple neuron)/plotConnectivityMatrix.m
@@ -1,12 +1,16 @@
 function [connectivitymatrix] = plotConnectivityMatrix(spiketrains)
 
+
+
+
     A = zeros(spiketrains);
     for i = 1:spiketrains
+        noise_matrix = 0.95 + (1 - 0.95) * rand();
         if i > 1
-            A(i, i-1) = 1; % Set the left adjacent element to 1
+            A(i, i-1) = 1*noise_matrix; % Set to number between 0.5 and 1
         end
         if i < spiketrains
-            A(i, i+1) = 1; % Set the right adjacent element to 1
+            A(i, i+1) = 1*noise_matrix; % Set to number between 0.5 and 1
         end
     end
     connectivitymatrix = A; 
